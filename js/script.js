f5d8be491769c657093267d80542ca92f180d963
Jose Iglesias <joseiglesiasarevalo@gmail.com>
2025-06-10 13:18:51 +0200

    Implementacion del cierre del menu desplegable al usar un enlace

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@ let lanzador = "#enlace-menu";
 let desplegable = "#menu";
 let despliegaClase = "menu-desplegado";
 
-function nav(){
+function nav() {
     let lanz = document.querySelector(lanzador);
     lanz.addEventListener("click", despliegaMenu);
+
+    let enlacesMenu = document.querySelectorAll(`${desplegable} a`);
+    enlacesMenu.forEach(enlace => {
+        enlace.addEventListener("click", () => {
+            let despl = document.querySelector(desplegable);
+            despl.classList.remove(despliegaClase);
+        });
+    });
 }
 
 function despliegaMenu(){
